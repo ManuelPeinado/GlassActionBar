fc40a451b841cc24dcd97e756df9ba08c793f7c0
Manuel Peinado <manuel.peinado@gmail.com>
2013-07-13 18:54:16 +0200

    Optimized view-to-bitmap conversion with canvas.scale

--- library/src/com/manuelpeinado/glassactionbar/Utils.java
+++ library/src/com/manuelpeinado/glassactionbar/Utils.java
@@ -6,18 +6,15 @@ import android.view.View;
 
 public class Utils {
     public static Bitmap drawViewToBitmap(View view, int width, int height, int downSampling) {
-        
-        // TODO use ony one bitmap if downSampling=1
-        
         int heightCopy = view.getHeight();
         view.layout(0, 0, width, height);
         Bitmap original = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(original);
+        if (downSampling > 1) {
+            c.scale(1f / downSampling, 1f / downSampling);
+        }
         view.draw(c);
-        Bitmap result = Bitmap.createScaledBitmap(original, width / downSampling, height / downSampling, true);
         view.layout(0, 0, width, heightCopy);
-        original.recycle();
-        return result;
+        return original;
     }
-
 }
