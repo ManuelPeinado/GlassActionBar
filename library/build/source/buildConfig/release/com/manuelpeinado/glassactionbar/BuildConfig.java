ed3fbe16fd3fb5fc609e79c5ec3c43111fc8a7ae
Manuel Peinado <manuel.peinado@gmail.com>
2013-09-29 18:24:23 +0200

    First steps towards new structure, base project now uses stock AB

---
+++ library/build/source/buildConfig/release/com/manuelpeinado/glassactionbar/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.manuelpeinado.glassactionbar;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = false;
+
+}
